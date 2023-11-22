9bfd1c9def964ea73303b93c677c75848a6843cd
imamulahsan <imamulahsan@hotmail.com>
2023-11-22 19:26:17 +0100

    login page design

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Form, Input, Button, Image, Typography, Divider } from 'antd';
+import yourImage from '../images/login-form-bg.jpg';
+import "../styles/LoginStyle.css"
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-        <h1>This is Login</h1>
+    <div className='login-container'>
+      <Row gutter={16}>
+        <Col xs={24} sm={12}>
+          <div className='image-container'>
+          <Image src={yourImage} alt="Description of the image" style={{boxShadow: '0px 8px 32px 0 rgba(4, 21, 148, 0.33)'}}/>
+          </div>
+        </Col>
+
+        <Col xs={24} sm={12}>
+        <div className='login-form-container'>
+        <Form
+          initialValues={{ remember: true }}
+          autoComplete="off"
+          className='login-form'>
+        <div className='login-form-text'>
+        <Typography style={{color: '#FDFEFE', textAlign:'center'}}>Melden Sie sich als Patient an</Typography>
+        <Typography.Title style={{color: '#FDFEFE', textAlign:'center'}}>Meine ARZT</Typography.Title>
+        <Divider style={{borderColor: "white", color: "white"}}>Persönliche Information</Divider>
+        </div>
+        <Form.Item label="E-Mail-Adresse" name="email" rules={[{ required: true, message: 'Please a valid email!' }]}>
+          <Input type='email' required></Input>
+        </Form.Item>
+        <Form.Item label="Passwort" name="password" rules={[{ required: true, message: 'Please input correct password!' }]}>
+          <Input.Password type='password' required></Input.Password>
+        </Form.Item>
+        <Button type="primary" htmlType="submit" block style={{backgroundColor:"#512E5F"}}>
+          Submit
+        </Button>
+        <Typography style={{padding: "10px" , textAlign: "center", margin: "5px"}}>Neuer Patient? <Link to="/register" className='form-text-below'>Hier registrieren</Link></Typography>
+      </Form>
+
+    </div>
+        </Col>
+      </Row>
     </div>
   )
 }
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button, Divider, Form, Input, Typography } from 'antd';
 import "../styles/RegisterStyle.css"
+import {Link} from 'react-router-dom';
 
 const onFinish = (values) => {
   console.log('Success:', values);
@@ -10,30 +11,36 @@ const onFinish = (values) => {
 const Register = () => {
   return (
     <>
-    <div className='form-container'>
+    <div className='reg-form-container'>
       <Form
         className='register-form'
         initialValues={{ remember: true }}
         onFinish={onFinish}
         autoComplete="off">
-        <div className='form-text'>
+        <div className='reg-form-text'>
         <Typography style={{color: '#FDFEFE', textAlign:'center'}}>Ihre Registrierung bei</Typography>
         <Typography.Title style={{color: '#FDFEFE', textAlign:'center'}}>Meine ARZT</Typography.Title>
         <Divider style={{borderColor: "white", color: "white"}}>Persönliche Information</Divider>
         </div>
-        <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please input your username!' }]}>
+        <Form.Item label="Name des Patienten" name="name" rules={[{ required: true, message: 'Please input your username!' }]}>
           <Input type='text' required></Input>
         </Form.Item>
-        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please a valid email!' }]}>
+        <Form.Item label="Adresse" name="address" rules={[{ required: true, message: 'Please input your address!' }]}>
+          <Input type='text' required></Input>
+        </Form.Item>
+        <Form.Item label="Geburtsdatum" name="dob" rules={[{ required: true, message: 'Please input your date of birth!' }]}>
+          <Input type='date' required></Input>
+        </Form.Item>
+        <Form.Item label="E-Mail-Adresse" name="email" rules={[{ required: true, message: 'Please a valid email!' }]}>
           <Input type='email' required></Input>
         </Form.Item>
-        <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your username!' }]}>
+        <Form.Item label="Passwort" name="password" rules={[{ required: true, message: 'Please input correct password!' }]}>
           <Input.Password type='password' required></Input.Password>
         </Form.Item>
-        <Typography style={{padding: "10px" , textAlign: "center", margin: "5px"}}>Already Registered? Login Instead</Typography>
         <Button type="primary" htmlType="submit" block style={{backgroundColor:"#512E5F"}}>
           Submit
         </Button>
+        <Typography style={{padding: "10px" , textAlign: "center", margin: "5px"}}>Hast du dich schon registriert? <Link to="/login" className='form-text-below'>Hier anmelden</Link></Typography>
       </Form>
 
     </div>
