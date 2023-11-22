f96b2acc617d52761b8533ab86af834ceb7cbb4b
imamulahsan <imamulahsan@hotmail.com>
2023-11-22 12:27:10 +0100

    register page design

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Form, Input } from 'antd';
+import { Button, Divider, Form, Input, Typography } from 'antd';
 import "../styles/RegisterStyle.css"
 
 const onFinish = (values) => {
@@ -12,24 +12,28 @@ const Register = () => {
     <>
     <div className='form-container'>
       <Form
-        layout='vertical'
+        className='register-form'
         initialValues={{ remember: true }}
         onFinish={onFinish}
         autoComplete="off">
+        <div className='form-text'>
+        <Typography style={{color: '#FDFEFE', textAlign:'center'}}>Ihre Registrierung bei</Typography>
+        <Typography.Title style={{color: '#FDFEFE', textAlign:'center'}}>Meine ARZT</Typography.Title>
+        <Divider style={{borderColor: "white", color: "white"}}>Persönliche Information</Divider>
+        </div>
         <Form.Item label="Name" name="name" rules={[{ required: true, message: 'Please input your username!' }]}>
           <Input type='text' required></Input>
         </Form.Item>
-        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input your email!' }]}>
+        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please a valid email!' }]}>
           <Input type='email' required></Input>
         </Form.Item>
         <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your username!' }]}>
-          <Input type='password' required></Input>
+          <Input.Password type='password' required></Input.Password>
         </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
+        <Typography style={{padding: "10px" , textAlign: "center", margin: "5px"}}>Already Registered? Login Instead</Typography>
+        <Button type="primary" htmlType="submit" block style={{backgroundColor:"#512E5F"}}>
           Submit
-          </Button>
-        </Form.Item>
+        </Button>
       </Form>
 
     </div>
