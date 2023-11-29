2422ca75ea7c16760dc4bc38039680abad1f47e6
imamulahsan <imamulahsan@hotmail.com>
2023-11-29 18:51:53 +0100

    authorization done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const {loginController, registerController} = require("../controllers/userController");
+const {loginController, registerController, authController} = require("../controllers/userController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 //router onject
 const router = express.Router();
@@ -10,4 +11,8 @@ router.post("/login", loginController);
 
 //REGISTER || POST
 router.post("/register", registerController);
+module.exports = router;
+
+//Auth || POST
+router.post("/getUserData", authMiddleware, authController);
 module.exports = router;
